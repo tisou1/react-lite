19323386737c6b7f1c62deb81ddc5ccf3da068a0
tisou1 <tisou1@qq.com>
2023-04-20 21:22:21 +0800

    chore: 更改swc和eslint

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,21 @@
 import * as path from 'path'
 import { defineConfig } from 'vite'
 //热更新模块插件
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 //文件路由
 import Pages from 'vite-plugin-pages'
 import Unocss from 'unocss/vite'
 //click to component
 export default defineConfig({
-  resolve:{
+  resolve: {
     alias: {
-      '~/': `${path.resolve(__dirname,'src')}/`,
-    }
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
   },
-  plugins: [
-    react(),
-    Unocss(),
-    Pages(),
-  ],
+  plugins: [react(), Unocss(), Pages()],
   test: {
     environment: 'jsdom',
     include: ['test/**/*.test.{ts,js}'],
-  }
+  },
 })
