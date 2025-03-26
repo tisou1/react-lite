59ec3c8f348aeb4462a0fd356b4138f8da16d052
tisou1 <57515396+tisou1@users.noreply.github.com>
2025-03-26 21:15:48 +0800

    chore: 添加圆形渐变效果

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,9 +4,48 @@ import { useDark } from '../hooks'
 export default function Footer() {
   const { isDark, toggleDark } = useDark()
 
+  const updateView = (event: React.MouseEvent<HTMLDivElement>) => {
+    // 在不支持的浏览器里不做动画
+    if (!document.startViewTransition) {
+      toggleDark()
+      return
+    }
+    // 开始一次视图过渡：
+    const transition = document.startViewTransition(() => toggleDark())
+    transition.ready.then(() => {
+      const x = event.clientX
+      const y = event.clientY
+      // 计算按钮到最远点的距离用作裁剪圆形的半径
+      const endRadius = Math.hypot(
+        Math.max(x, innerWidth - x),
+        Math.max(y, innerHeight - y),
+      )
+
+      const clipPath = [
+        `circle(0px at ${x}px ${y}px)`,
+        `circle(${endRadius}px at ${x}px ${y}px)`,
+      ]
+      // 开始动画
+      document.documentElement.animate(
+        {
+          // 这里使用!isDark, 是因为当那个我们这个组件拿到isDark时, isDark的值已经更新了,
+          // 所以需要使用!isDark来表示主题改变之前的值,按照之前的值来进行动画
+          clipPath: !isDark ? clipPath.reverse() : clipPath,
+        },
+        {
+          duration: 400,
+          easing: 'ease-in',
+          pseudoElement: !isDark
+            ? '::view-transition-old(root)'
+            : '::view-transition-new(root)',
+        },
+      )
+    })
+  }
+
   return (
     <div className="text-center py-6 flex justify-center">
-      <div onClick={toggleDark}>
+      <div onClick={updateView}>
         {
           isDark
             ? <span className="i-carbon-moon text-gray-200"></span>
--- src/hooks/useDark.tsx
+++ src/hooks/useDark.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import useLocalStorage from './useLocalStorage'
 
 function getSystemTheme() {
@@ -8,6 +8,13 @@ function getSystemTheme() {
   return themeMedia
 }
 
+function getTheme() {
+  const theme = localStorage.getItem('si-theme')
+  if (!theme)
+    return getSystemTheme() ? 'dark' : 'light'
+  return theme
+}
+
 type Theme = 'dark' | 'light' | 'auto'
 
 export default function useDark(): {
@@ -15,24 +22,30 @@ export default function useDark(): {
   setDark: (value: Theme) => void
   toggleDark: () => void
 } {
-  const [theme, setTheme] = useLocalStorage('si-theme', 'auto')
-  const isDark = theme === 'auto' ? getSystemTheme() : theme === 'dark'
+  // const [theme, setTheme] = useLocalStorage('si-theme', 'auto')
+  const [theme, setTheme] = useState(getTheme())
 
   useEffect(() => {
     // isDark将会添加'dark'类名, 为false将会清除'dark'类名'
-    document.documentElement.classList.toggle('dark', isDark)
-  }, [isDark])
+    document.documentElement.classList.toggle('dark', theme === 'dark')
+    localStorage.setItem('si-theme', theme as string)
+  }, [])
 
   const setDark = (value: Theme) => {
+    // 更新类名
+    document.documentElement.classList.toggle('dark', value === 'dark')
+    // 更新本地存储
+    localStorage.setItem('si-theme', value)
+    // 更新stater
     setTheme(value)
   }
 
   const toggleDark = () => {
-    setTheme(isDark ? 'light' : 'dark')
+    setDark(theme === 'dark' ? 'light' : 'dark')
   }
 
   return {
-    isDark,
+    isDark: theme === 'dark',
     setDark,
     toggleDark,
   }
